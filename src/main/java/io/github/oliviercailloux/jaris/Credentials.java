ffc553a49b0c7ee5b2169cce1e7f6662169a39bc
Olivier Cailloux <olivier.cailloux@dauphine.fr>
2020-07-12 19:37:07 +0200

    Completed Credentials

--- src/main/java/io/github/oliviercailloux/jaris/Credentials.java
+++ src/main/java/io/github/oliviercailloux/jaris/Credentials.java
@@ -2,15 +2,17 @@ package io.github.oliviercailloux.jaris;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.util.Optional;
+import java.util.Objects;
+
+import com.google.common.base.MoreObjects;
 
 /**
  * <p>
  * Immutable.
  * </p>
  * <p>
- * Stores two Strings of login information: username and password. The value can
- * be an empty String but never <code>null</code>.
+ * Stores two pieces of login information as String: username and password. Each
+ * of these values can be an empty String but never <code>null</code>.
  * </p>
  */
 public class Credentials {
@@ -29,7 +31,7 @@ public class Credentials {
 	}
 
 	/**
-	 * 
+	 *
 	 * @return The value of the username which can be an empty String but never
 	 *         <code>null</code>.
 	 */
@@ -38,12 +40,32 @@ public class Credentials {
 	}
 
 	/**
-	 * 
+	 *
 	 * @return The value of the password which can be an empty String but never
 	 *         <code>null</code>.
 	 */
 	public String getPassword() {
 		return password;
+	}
+
+	@Override
+	public boolean equals(Object o2) {
+		if (!(o2 instanceof Credentials)) {
+			return false;
+		}
+		final Credentials c2 = (Credentials) o2;
+		return username.equals(c2.username) && password.equals(c2.password);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(username, password);
+	}
+
+	@Override
+	public String toString() {
+		return MoreObjects.toStringHelper(this).add("username", username)
+				.add("password", password.isEmpty() ? "" : "****").toString();
 	}
 
 }
