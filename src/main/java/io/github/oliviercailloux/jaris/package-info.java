aefe2d5f7a6c2d851d7f3d99dc3795a8d2714828
Olivier Cailloux <olivier.cailloux@gmail.com>
2021-04-15 20:40:13 +0200

    Note null

---
+++ src/main/java/io/github/oliviercailloux/jaris/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Every methods in this library are
+ * <a href="https://github.com/google/guava/wiki/UsingAndAvoidingNullExplained">null hostile</a>:
+ * they throw {@link java.lang.NullPointerException} if given {@code null} arguments, and never
+ * return {@code null} values.
+ */
+package io.github.oliviercailloux.jaris;
