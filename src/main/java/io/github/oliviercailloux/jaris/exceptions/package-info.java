57f26456524fda2a5378264887fb32fbb0419ca0
Olivier Cailloux <olivier.cailloux@gmail.com>
2021-05-02 23:24:58 +0200

    Added pck-info

---
+++ src/main/java/io/github/oliviercailloux/jaris/exceptions/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Objects to deal with code that declare checked exceptions; or that throw exceptions.
+ */
+package io.github.oliviercailloux.jaris.exceptions;
--- src/main/java/io/github/oliviercailloux/jaris/package-info.java
+++ src/main/java/io/github/oliviercailloux/jaris/package-info.java
@@ -1,7 +1,9 @@
 /**
  * A collection of various helpers and types that are felt to miss from the JDK, with a focus on
  * best practices and well-designed API, similar to
- * <a href="https://github.com/google/guava">Guava</a> in spirit.
+ * <a href="https://github.com/google/guava">Guava</a> in spirit. See also the
+ * <a href="https://github.com/oliviercailloux/JARiS/blob/master/README.adoc">readme</a> of this
+ * library.
  *
  * <p>
  * Every methods in this library are
