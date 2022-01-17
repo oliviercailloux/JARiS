b51d173738d76fd0618aa590b06fe572f3e97c14
Olivier Cailloux <olivier.cailloux@gmail.com>
2022-01-17 14:45:17 +0100

    Phrasing doc XmlUtils

--- src/main/java/io/github/oliviercailloux/jaris/xml/XmlUtils.java
+++ src/main/java/io/github/oliviercailloux/jaris/xml/XmlUtils.java
@@ -79,9 +79,9 @@ import org.xml.sax.SAXParseException;
  * <p>
  * The public API of this class favors {@link StreamSource} (from {@code javax.xml.transform}) to
  * {@link InputSource} (from {@code org.xml.sax}). Both classes come from the {@code java.xml}
- * module, and their APIs are almost identical, the only difference being that {@code InputSource}
- * has an “encoding” parameter; and that {@code StreamSource} is part of a hierarchy (as it
- * implements {@link Source}), which makes it nicer to use in this context. See also
+ * module, and their APIs are almost identical, the only differences being that {@code StreamSource}
+ * is part of a hierarchy (as it implements {@link Source}), which makes it nicer to use in this
+ * context; and that {@code InputSource} has an “encoding” parameter, which we do not need. See also
  * <a href="https://stackoverflow.com/q/69194590">SO</a>.
  * </p>
  */
