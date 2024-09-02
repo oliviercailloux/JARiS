9ec733367c9d2344a114b27cebf6faf22a35b8e7
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-09-02 23:52:05 +0200

    Mv GraphUtils to own package

--- src/main/java/io/github/oliviercailloux/jaris/graphs/GraphUtils.java
+++ src/main/java/io/github/oliviercailloux/jaris/graphs/GraphUtils.java
@@ -1,4 +1,4 @@
-package io.github.oliviercailloux.jaris.collections;
+package io.github.oliviercailloux.jaris.graphs;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
