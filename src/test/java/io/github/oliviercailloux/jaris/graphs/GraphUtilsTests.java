805d5ca9900f28d13d96bdf98cfa31258812ebe4
Olivier Cailloux <olivier.cailloux@gmail.com>
2024-09-02 23:55:11 +0200

    Really mv graph utils tests

--- src/test/java/io/github/oliviercailloux/jaris/graphs/GraphUtilsTests.java
+++ src/test/java/io/github/oliviercailloux/jaris/graphs/GraphUtilsTests.java
@@ -1,4 +1,4 @@
-package io.github.oliviercailloux.jaris.collections;
+package io.github.oliviercailloux.jaris.graphs;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
