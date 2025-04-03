c6c47a8d8076073ea2fc3d8c4deb284cfccc2a82
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-04-03 20:06:26 +0200

    CheckedStreamFactory (beta)

---
+++ src/main/java/io/github/oliviercailloux/jaris/exceptions/CheckedStreamFactory.java
@@ -0,0 +1,22 @@
+package io.github.oliviercailloux.jaris.exceptions;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+public class CheckedStreamFactory<X extends Exception> {
+  public static <X extends Exception> CheckedStreamFactory<X> instance() {
+    return new CheckedStreamFactory<>();
+  }
+
+  private CheckedStreamFactory() {
+    // Private constructor to prevent instantiation
+  }
+
+  public <T> CheckedStream<T, X> wrapping(Stream<T> delegate) {
+    return CheckedStreamImpl.wrapping(delegate);
+  }
+  public <T> CheckedStream<T, X> from(Collection<T> collection) {
+    return wrapping(collection.stream());
+  }
+  
+}
