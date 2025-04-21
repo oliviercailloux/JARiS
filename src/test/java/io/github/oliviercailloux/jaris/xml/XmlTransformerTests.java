3311d0930a78687ad753769761f0fc9ba5568e88
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-04-21 19:27:36 +0200

    attempt set proxy

--- src/test/java/io/github/oliviercailloux/jaris/xml/XmlTransformerTests.java
+++ src/test/java/io/github/oliviercailloux/jaris/xml/XmlTransformerTests.java
@@ -33,7 +33,8 @@ import org.junitpioneer.jupiter.SetSystemProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class XmlTransformerTests {
+  // @SetSystemProperty(key = "https.proxyHost", value = "invalid.invalid")
+  class XmlTransformerTests {
   @SuppressWarnings("unused")
   private static final Logger LOGGER = LoggerFactory.getLogger(XmlTransformerTests.class);
 
@@ -138,6 +139,7 @@ class XmlTransformerTests {
   @ParameterizedTest
   @EnumSource(names = {"XALAN", "SAXON"})
   void testMissInternet(KnownFactory factory) throws Exception {
+    ProxySelector.setDefault(new DenierProxy());
     final URI myStyle = new URI("https://cdn.docbook.org/release/xsl/1.79.2/fo/docbook.xsl");
 
     XmlException exc = assertThrows(XmlException.class,
@@ -149,6 +151,9 @@ class XmlTransformerTests {
   @ParameterizedTest
   @EnumSource(names = {"XALAN", "SAXON"})
   void testDocBookSimple(KnownFactory factory) throws Exception {
+    ProxySelector.setDefault(new DenierProxy());
+    final URI myStyle =
+        URI.create("http://docbook.sourceforge.net/release/xsl/current/fo/docbook.xsl");
     final CharSource docBook = charSource("DocBook/Simple.xml");
 
     final String transformed = XmlTransformerFactory.usingFactory(withDocBookResolver(factory))
@@ -160,6 +165,7 @@ class XmlTransformerTests {
   @ParameterizedTest
   @EnumSource(names = {"XALAN", "SAXON"})
   void testDocBookComplex(KnownFactory factory) throws Exception {
+    ProxySelector.setDefault(null);
     final CharSource docBook = charSource("DocBook/Howto.xml");
     final CharSource myStyle = charSource("DocBook/mystyle.xsl");
 
