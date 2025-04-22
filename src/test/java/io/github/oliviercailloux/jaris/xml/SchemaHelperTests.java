a967b78f5285b92ecdae3645afaf152166b28866
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-04-22 18:47:00 +0200

    Using embedded docbook xsd

--- src/test/java/io/github/oliviercailloux/jaris/xml/SchemaHelperTests.java
+++ src/test/java/io/github/oliviercailloux/jaris/xml/SchemaHelperTests.java
@@ -1,9 +1,12 @@
 package io.github.oliviercailloux.jaris.xml;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.google.common.base.VerifyException;
+import com.google.common.io.Resources;
 import java.net.URI;
+import java.net.URL;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import org.junit.jupiter.api.Test;
@@ -12,10 +15,10 @@ class SchemaHelperTests {
 
   @Test
   void testValidDocBook() throws Exception {
-    final StreamSource docBook = new StreamSource(
-        XmlTransformerTests.class.getResource("DocBook/Simple.xml").toString());
-    final URI docBookSchemaSource =
-        URI.create("https://cdn.docbook.org/schema/5.0.1/xsd/docbook.xsd");
+    final URI docBookSchemaSource = Resources
+        .getResource(getClass(), "/org/docbook/schemas/docbook/5.0/xsd/docbook.xsd").toURI();
+    final StreamSource docBook =
+        new StreamSource(XmlTransformerTests.class.getResource("DocBook/Simple.xml").toString());
 
     final SchemaHelper schemaHelper = SchemaHelper.schemaHelper();
     final Schema docBookSchema = schemaHelper.asSchema(docBookSchemaSource);
@@ -26,10 +29,10 @@ class SchemaHelperTests {
 
   @Test
   void testInvalidDocBook() throws Exception {
-    final StreamSource docBook = new StreamSource(
-        XmlTransformerTests.class.getResource("DocBook/Invalid.xml").toString());
-    final URI docBookSchemaSource =
-        URI.create("https://cdn.docbook.org/schema/5.0.1/xsd/docbook.xsd");
+    final URI docBookSchemaSource = Resources
+        .getResource(getClass(), "/org/docbook/schemas/docbook/5.0/xsd/docbook.xsd").toURI();
+    final StreamSource docBook =
+        new StreamSource(XmlTransformerTests.class.getResource("DocBook/Invalid.xml").toString());
 
     final SchemaHelper schemaHelper = SchemaHelper.schemaHelper();
     final Schema docBookSchema = schemaHelper.asSchema(docBookSchemaSource);
