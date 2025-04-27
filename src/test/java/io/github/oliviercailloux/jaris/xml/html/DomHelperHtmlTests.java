2bcbba4550094e115c5fa804135403da5a3ce6bb
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-04-28 01:09:23 +0200

    Test no Html support

---
+++ src/test/java/io/github/oliviercailloux/jaris/xml/html/DomHelperHtmlTests.java
@@ -0,0 +1,59 @@
+package io.github.oliviercailloux.jaris.xml.html;
+
+import static io.github.oliviercailloux.jaris.xml.Resourcer.byteSource;
+import static io.github.oliviercailloux.jaris.xml.Resourcer.charSource;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.google.common.base.Throwables;
+import com.google.common.io.ByteSource;
+import com.google.common.io.CharSource;
+import io.github.oliviercailloux.jaris.xml.DomHelper;
+import io.github.oliviercailloux.jaris.xml.XmlException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.apache.html.dom.HTMLDOMImplementationImpl;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+import org.w3c.dom.html.HTMLDOMImplementation;
+import org.w3c.dom.html.HTMLDocument;
+import org.xml.sax.SAXParseException;
+
+public class DomHelperHtmlTests {
+  @Test
+  public void testSimpleDomHelper() throws Exception {
+    CharSource simple = charSource("Html/Simple.html");
+    DomHelper h = DomHelper.domHelper();
+    XmlException e = assertThrows(XmlException.class, () -> h.asDocument(simple));
+    String message = Throwables.getRootCause(e).getMessage();
+    assertTrue(message.contains("/meta"), message);
+  }
+
+  @Test
+  public void testSimpleDocBuilder() throws Exception {
+    ByteSource simple = byteSource("Html/Simple.html");
+    final DocumentBuilderFactory factory = DocumentBuilderFactory.newNSInstance();
+    final DocumentBuilder builder = factory.newDocumentBuilder();
+    try (InputStream is = simple.openStream()) {
+      SAXParseException e = assertThrows(SAXParseException.class, () -> builder.parse(is));
+      String message = Throwables.getRootCause(e).getMessage();
+      assertTrue(message.contains("/meta"), message);
+    }
+  }
+
+  @Test
+  public void testNoHtmlFeature() throws Exception {
+    final DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
+    assertFalse(registry.getDOMImplementation("Core").hasFeature("HTML", "1.0"));
+    /* https://www.w3.org/TR/2003/REC-DOM-Level-2-HTML-20030109/html.html#ID-1176245063 */
+    assertFalse(registry.getDOMImplementation("Core").hasFeature("HTML", "2.0"));
+    assertFalse(registry.getDOMImplementation("XML").hasFeature("HTML", "2.0"));
+    assertNull(registry.getDOMImplementation("HTML"));
+  }
+}
