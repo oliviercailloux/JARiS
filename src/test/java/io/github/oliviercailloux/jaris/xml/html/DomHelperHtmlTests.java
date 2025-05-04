cd520a8b6aeb67e865f08b2c32bf09d55597e952
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-05-04 23:31:40 +0200

    tests xerces

--- src/test/java/io/github/oliviercailloux/jaris/xml/html/DomHelperHtmlTests.java
+++ src/test/java/io/github/oliviercailloux/jaris/xml/html/DomHelperHtmlTests.java
@@ -2,6 +2,7 @@ package io.github.oliviercailloux.jaris.xml.html;
 
 import static io.github.oliviercailloux.jaris.xml.Resourcer.byteSource;
 import static io.github.oliviercailloux.jaris.xml.Resourcer.charSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -11,18 +12,26 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.google.common.base.Throwables;
 import com.google.common.io.ByteSource;
 import com.google.common.io.CharSource;
+import com.google.common.io.Resources;
 import io.github.oliviercailloux.jaris.xml.DomHelper;
+import io.github.oliviercailloux.jaris.xml.Resourcer;
 import io.github.oliviercailloux.jaris.xml.XmlException;
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.apache.html.dom.HTMLBuilder;
 import org.apache.html.dom.HTMLDOMImplementationImpl;
+import org.apache.xerces.parsers.DOMParser;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.bootstrap.DOMImplementationRegistry;
 import org.w3c.dom.html.HTMLDOMImplementation;
 import org.w3c.dom.html.HTMLDocument;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXParseException;
 
 public class DomHelperHtmlTests {
@@ -38,13 +47,32 @@ public class DomHelperHtmlTests {
   @Test
   public void testSimpleDocBuilder() throws Exception {
     ByteSource simple = byteSource("Html/Simple.html");
-    final DocumentBuilderFactory factory = DocumentBuilderFactory.newNSInstance();
-    final DocumentBuilder builder = factory.newDocumentBuilder();
+    final DocumentBuilder builderNs = DocumentBuilderFactory.newNSInstance().newDocumentBuilder();
+    try (InputStream is = simple.openStream()) {
+      SAXParseException e = assertThrows(SAXParseException.class, () -> builderNs.parse(is));
+      String message = Throwables.getRootCause(e).getMessage();
+      assertTrue(message.contains("/meta"), message);
+    }
+    final DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
     try (InputStream is = simple.openStream()) {
       SAXParseException e = assertThrows(SAXParseException.class, () -> builder.parse(is));
       String message = Throwables.getRootCause(e).getMessage();
       assertTrue(message.contains("/meta"), message);
     }
+  }
+
+  @Test
+  public void testDirectDocBuilder() throws Exception {
+    ByteSource simple = byteSource("Html/Simple.html");
+    DOMParser parser = new DOMParser();
+    try (InputStream is = simple.openStream()) {
+      SAXParseException e =
+          assertThrows(SAXParseException.class, () -> parser.parse(new InputSource(is)));
+      String message = Throwables.getRootCause(e).getMessage();
+      assertTrue(message.contains("/meta"), message);
+    }
+    // URL input = Resources.getResource(Resourcer.class, "Html/Simple.html");
+    // parser.parse(input.toString());
   }
 
   @Test
@@ -55,5 +83,23 @@ public class DomHelperHtmlTests {
     assertFalse(registry.getDOMImplementation("Core").hasFeature("HTML", "2.0"));
     assertFalse(registry.getDOMImplementation("XML").hasFeature("HTML", "2.0"));
     assertNull(registry.getDOMImplementation("HTML"));
+  }
+
+  @Test
+  public void testCreateNotProperHtml() throws Exception {
+    HTMLDOMImplementation d = HTMLDOMImplementationImpl.getHTMLDOMImplementation();
+    assertNotNull(d);
+    HTMLDocument doc = d.createHTMLDocument("Ze title");
+    String ser = DomHelper.domHelper().toString(doc);
+    // Files.writeString(Path.of("zetitle.html"), ser);
+    assertFalse(ser.contains("DOCTYPE"));
+  }
+
+  @SuppressWarnings("deprecation")
+  @Test
+  public void testSaxIncomplete() throws Exception {
+    SAXParserFactory factory = SAXParserFactory.newInstance();
+    SAXParser saxParser = factory.newSAXParser();
+    saxParser.getParser().setDocumentHandler(new HTMLBuilder());
   }
 }
