db4204d7922b07cccb4257a9294663890ca85b17
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-06-18 23:42:56 +0200

    rm more annot

--- src/main/java/io/github/oliviercailloux/jaris/credentials/Credentials.java
+++ src/main/java/io/github/oliviercailloux/jaris/credentials/Credentials.java
@@ -1,6 +1,5 @@
 package io.github.oliviercailloux.jaris.credentials;
 
-@SuppressWarnings({"checkstyle:AbbreviationAsWordInName", "checkstyle:RecordComponentName"})
 public record Credentials (String API_USERNAME, String API_PASSWORD) {
 
 }
