03d58074bc45f528b627c88795d19fdd9c06ae82
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-07-07 08:40:23 +0200

    res streamsource

--- src/test/java/io/github/oliviercailloux/jaris/xml/Resourcer.java
+++ src/test/java/io/github/oliviercailloux/jaris/xml/Resourcer.java
@@ -10,16 +10,25 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import javax.xml.transform.stream.StreamSource;
 
 public class Resourcer {
+  public static URL url(String resourceName) {
+    return Resources.getResource(Resourcer.class, resourceName);
+  }
+
   public static ByteSource byteSource(String resourceName) {
-    return Resources.asByteSource(Resourcer.class.getResource(resourceName));
+    return Resources.asByteSource(url(resourceName));
   }
 
   public static CharSource charSource(String resourceName) {
     return byteSource(resourceName).asCharSource(StandardCharsets.UTF_8);
   }
 
+  public static StreamSource streamSource(String resourceName) {
+    return new StreamSource(url(resourceName).toString());
+  }
+  
   public static CharSource charSource(URL source) {
     return Resources.asCharSource(source, StandardCharsets.UTF_8);
   }
